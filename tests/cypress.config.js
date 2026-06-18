3544de1d096cdd062988bde39720c9926d2e6a0c
Juan Carlos De la Cruz Corrales <juan.delacruzco@usil.pe>
2026-06-17 21:52:28 -0500

    correccion gitactions cypress iniciar servidor

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost/eduka',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost/eduka',
     specPattern: 'cypress/e2e/**/*.cy.js',
     setupNodeEvents(on, config) {},
   },
